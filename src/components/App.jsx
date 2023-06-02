c88281e14ba9d1632a66f229dc154fff6b83e95b
TaniaTerzi <terzitetyana@gmail.com>
2023-06-02 15:47:59 +0200

    create form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
+import React, { Component } from "react";
+
+export  class App extends Component {
+  state = {
+    inputValue: "",
+  };
+
+  handleChange = evt => {
+    this.setState({ inputValue: evt.target.value });
+  };
+
+  render() {
+    const { inputValue } = this.state;
+    return (
+      <input type="text" value={inputValue} onChange={this.handleChange} />
+    );
+  }
+}
